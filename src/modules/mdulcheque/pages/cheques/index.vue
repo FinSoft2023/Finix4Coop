3b5b318c256c7dfb2c919193d6d01df0fa7744a8
Jane1m <rinda@finsoft2023.com>
2024-08-06 01:35:32 +0700

    delete button New > #18

--- src/modules/mdulcheque/pages/cheques/index.vue
+++ src/modules/mdulcheque/pages/cheques/index.vue
@@ -4,28 +4,30 @@
 
     <BPartPageBody>
       <BPartButtonsBand>
-        <UButton to="/cheques/create" icon="i-heroicons-plus-circle"
-          >New</UButton
-        >
         <template #next>
           <DSmartTabs />
         </template>
       </BPartButtonsBand>
-      <FTableOfTxs @selection-changed="selectItem" :data :pending />
+      <FTableOfTxs @selection-changed="selectItem"
+        :data
+        :pending />
       <BPartSectionTitle>{{ pageDef.label }}</BPartSectionTitle>
-      <DTable @selection-changed="selectItem" :data :pending />
+      <DTable @selection-changed="selectItem"
+        :data
+        :pending />
     </BPartPageBody>
   </BFullPage>
 </template>
 
-<script setup lang="ts">
-const pageDef = useActiveModulePage('list.root');
-useBreadcrumb('List');
+<script setup
+  lang="ts">
+  const pageDef = useActiveModulePage('list.root');
+  useBreadcrumb('List');
 
-const { apiGet } = useHostApi(pageDef);
-const { data, error, pending } = apiGet();
+  const { apiGet } = useHostApi(pageDef);
+  const { data, error, pending } = apiGet();
 
-function selectItem(item: any) {
-  navigateTo(`/cheques/${item.id}`);
-}
+  function selectItem(item: any) {
+    navigateTo(`/cheques/${item.id}`);
+  }
 </script>
