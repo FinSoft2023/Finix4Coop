efaa6daa67eab0b116a9b3b3f4dd4b93c8c93780
Jane1m <rinda@finsoft2023.com>
2024-08-06 20:58:14 +0700

    UI Line  Deposit money  #38

--- src/modules/mdultransferonline/pages/transferonline/[id]/index.vue
+++ src/modules/mdultransferonline/pages/transferonline/[id]/index.vue
@@ -3,23 +3,39 @@
     <BPartPageTitle>{{ pageDef.label }}</BPartPageTitle>
 
     <BPartPageBody>
-      <UCard>
-        <DEntitySection v-model="data" :entries :pending />
-      </UCard>
+      <div class="flex justify-center">
+        <Icon class="w-20 h-20 text-green-400"
+          name="line-md:confirm-circle"></Icon>
+      </div>
+      <h2 class="mt-4 mb-4 text-2xl text-center font-bold dark:text-white">
+        ทำรายการฝากเงินเสร็จสิ้น
+      </h2>
+      <h3 class="text-center text-gray-500 dark:text-gray-300 ">
+        กลับไปยังหน้าแรกเพื่อตรวจสอบยอดเงิน
+      </h3>
+      <div class="flex justify-center">
+        <UButton to="/transferonline/">Home</UButton>
+      </div>
+      <!-- <UCard>
+        <DEntitySection v-model="data"
+          :entries
+          :pending />
+      </UCard> -->
     </BPartPageBody>
 
-    <template #side>
+    <!-- <template #side>
       <DSubLinks />
-    </template>
+    </template> -->
   </BFullPage>
 </template>
 
-<script setup lang="ts">
-const pageDef = useActiveModulePage('each.root');
+<script setup
+  lang="ts">
+  const pageDef = useActiveModulePage('each.root');
 
-const { entries } = getEntrySchema(pageDef);
-const { apiGet } = useHostApi(pageDef);
-const { data, error, pending } = apiGet();
+  const { entries } = getEntrySchema(pageDef);
+  const { apiGet } = useHostApi(pageDef);
+  const { data, error, pending } = apiGet();
 
-useBreadcrumb(pageDef.label);
+  useBreadcrumb(pageDef.label);
 </script>
--- src/modules/mdultransferonline/pages/transferonline/create/qrcode.vue
+++ src/modules/mdultransferonline/pages/transferonline/create/qrcode.vue
@@ -4,30 +4,29 @@
 
     <DSmartSubStepper />
 
-    <UAlert
-      icon="i-heroicons-book-open"
-      description="ระบุรายละเอียดขั้นตอนการทำงาน"
-      :title="pageDef.label"
-    />
+    <UAlert icon="i-heroicons-book-open"
+      description="QR Code โอนเงิน"
+      :title="pageDef.label" />
 
     <BPartPageBody>
-      <UForm
-        @submit="handleSubmit"
+      <UForm @submit="handleSubmit"
         :state="data"
         :schema="schema"
         :pending="pending"
-        class="space-y-4"
-      >
-        <DItemGrid col="x3">
-          <UCard class="col-span-2">
-            <DEntitySection v-model="data" :entries :pending />
-          </UCard>
-          <UCard> Your content here </UCard>
-        </DItemGrid>
+        class="space-y-4">
+        <UCard class="flex justify-center">
+          <img :src="qrCode"
+            alt="QR Code"
+            class="flex justify-center max-w-full shadow-md rounded-lg"
+            style="width: 200px; height: 200px" />
+        </UCard>
 
         <BPartButtonsBand>
+          <UButton @click="$router.back"
+            icon="i-heroicons-chevron-left-16-solid"
+            variant="outline">Back</UButton>
           <template #next>
-            <UButton type="submit">Save</UButton>
+            <UButton type="submit">comfirm</UButton>
           </template>
         </BPartButtonsBand>
       </UForm>
@@ -35,26 +34,27 @@
   </BFullPage>
 </template>
 
-<script setup lang="ts">
-// import type { z } from 'zod';
+<script setup
+  lang="ts">
+  // import type { z } from 'zod';
 
-const pageDef = useActiveModulePage('create.qrcode');
-useSmartStepper(pageDef);
-useBreadcrumb('Create');
+  const pageDef = useActiveModulePage('create.qrcode');
+  useSmartStepper(pageDef);
+  useBreadcrumb('Create');
 
-const { entries, schema } = getEntrySchema(pageDef);
-// type TSchema = z.output<typeof schema>;
+  const { entries, schema } = getEntrySchema(pageDef);
+  // type TSchema = z.output<typeof schema>;
 
-const { apiGet, apiPost } = useLocalStage(pageDef);
-const { data, pending } = apiGet();
-const { postResult, error, executePost } = apiPost();
+  const { apiGet, apiPost } = useLocalStage(pageDef);
+  const { data, pending } = apiGet();
+  const { postResult, error, executePost } = apiPost();
 
-// If this is the first step, you can initialize the data value like this:
-// data.value = {};
+  // If this is the first step, you can initialize the data value like this:
+  // data.value = {};
 
-const handleSubmit = async () => {
-  await executePost(data.value);
-  const redirectPath = getNextStep(pageDef);
-  navigateTo(`/transferonline/create/${redirectPath}`);
-};
+  const handleSubmit = async () => {
+    await executePost(data.value);
+    const redirectPath = getNextStep(pageDef);
+    navigateTo(`/transferonline/create/${redirectPath}`);
+  };
 </script>
