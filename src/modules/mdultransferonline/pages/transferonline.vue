1a3409f6c15cea7628a9ee2eeda82232f8b4654c
baramee0191 <nobpharut@finsoft2023.com>
2024-08-06 19:45:01 +0700

    Create Module Deposit money online #41

---
+++ src/modules/mdultransferonline/pages/transferonline.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script setup lang="ts">
+const moduleRef = useTransferonlineModule();
+setModulePageRoot([
+  {
+    label: 'Home',
+    icon: 'i-heroicons-home',
+    to: '/',
+  },
+  {
+    label: 'Transferonline',
+    icon: 'i-heroicons-square-3-stack-3d',
+    to: '/transferonline',
+  },
+]);
+useSmartTabs([
+  {
+    label: 'Active',
+    to: '/transferonline',
+  },
+  {
+    label: 'Closed',
+    to: '/transferonline/closed',
+  },
+]);
+</script>
