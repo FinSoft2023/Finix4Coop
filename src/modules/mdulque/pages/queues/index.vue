d1188bc54084c18d6fcf0776b8674c6ef0056db9
Teerachai Laothong <teerachai@finsoft2023.com>
2024-08-07 01:43:22 +0700

    feat: Add 'เรียก Queue' link to index page and update Stepper component to display step labels

--- src/modules/mdulque/pages/queues/index.vue
+++ src/modules/mdulque/pages/queues/index.vue
@@ -2,41 +2,71 @@
   <BFullPage>
     <BPartPageTitle>{{ pageDef.label }}</BPartPageTitle>
     <BPartPageBody>
-      <BPartButtonsBand> </BPartButtonsBand>
+      <BPartButtonsBand>
+        <UButton icon="i-heroicons-user-plus-16-solid"
+          to="/queues/create/steps"
+          target="_blank"
+          class="mr-2">Monitor</UButton>
+      </BPartButtonsBand>
+      <DItemGrid v-if="data.length"
+        col="x4">
+        <UPageCard icon="i-heroicons-user-plus-16-solid"
+          title="เรียกคิว"
+          description="เรียกคิวลูกค้าเพื่อให้บริการ"
+          to="/queues/create" />
+        <UPageCard icon="i-heroicons-forward-16-solid"
+          title="ข้ามคิว"
+          description="ข้ามคิวลูกค้าคนถัดไป"
+          to="/queues/create" />
 
-      
+        <UCard class="col-span-2">
+          <h2 class="mt-4 mb-4 text-4xl text-center font-bold dark:text-white">A001</h2>
+          <h2 class="mt-4 mb-4 text-2xl text-center dark:text-white">ถอน</h2>
+        </UCard>
+      </DItemGrid>
 
-      <div class="grid grid-cols-2 md:grid-cols-5 gap-4">
-        <div></div>
-        <div class="col-span-2 md:col-span-3">
-          <UCard>
-            <h2 class="mt-4 mb-4 text-2xl text-center font-bold dark:text-white">คิวที่</h2>
-            <h2 class="mt-4 mb-4 text-4xl text-center font-bold dark:text-white">A00{{ idx + 1 }}</h2>
-            <h2 class="mt-4 mb-4 text-4xl text-center font-bold dark:text-white">{{ user?.name }} ไม่พบข้อมูล</h2>
-            <h2 class="mt-4 mb-4 text-2xl text-center dark:text-white">{{ item?.type }}ถอน</h2>
-            <div class="grid grid-cols-2 gap-1 mx-auto max-w-lg">
-              <UButton to="/queues/create" icon="i-heroicons-plus-circle" variant="solid" block >เรียกคิว</UButton>
-              <UButton to="/" icon="i-heroicons-forward" variant="solid" block>คิวถัดไป</UButton>
-            </div>
-          </UCard>
-        </div>
-        <div></div>
-        <div></div>
-      </div>
-      <div></div>
       <!-- <DTable @selection-changed="selectItem" :data :pending /> -->
     </BPartPageBody>
   </BFullPage>
 </template>
 
 <script setup lang="ts">
+import * as Ably from 'ably';
+
 const pageDef = useActiveModulePage('list.root');
 useBreadcrumb('List');
 
 const { apiGet } = useHostApi(pageDef);
-const { data, error, pending } = apiGet();
+const { data, error, pending, execute } = apiGet({ 'fltr-val': 'called' });
 
-function selectItem(item: any) {
-  navigateTo(`/queues/${item.id}`);
+// For the full code sample see here: https://github.com/ably/quickstart-js
+const ably = new Ably.Realtime('9CNytA.ZRMqIg:YpI5Z9A8atb0cjkvlCvvGS8vvx8jg1clvIT6a0fhG_s');
+await ably.connection.once('connected');
+console.log('Connected to Ably!');
+
+const imageUrl = ref('');
+const route = useRoute();
+async function handleConfirmation() {
+  await execute();
 }
+
+// get the channel to subscribe to
+const channel = ably.channels.get('quickstart');
+
+const incoming = ref<any>();
+
+/*
+  Subscribe to a channel.
+  The promise resolves when the channel is attached
+  (and resolves synchronously if the channel is already attached).
+*/
+await channel.subscribe('greeting', (message) => {
+  console.log('Received a message: ', message.data);
+  incoming.value = message.data;
+  handleConfirmation();
+});
+
+// function selectItem(item: any) {
+//   navigateTo(`/queues/${item.id}`);
+// }
 </script>
