7eb86ebd2e76af19a274247221ec96fc40a535e4
Teerachai Laothong <teerachai@finsoft2023.com>
2024-08-06 18:44:52 +0700

    feat: Implement real-time payment confirmation using Ably

--- src/modules/mdulregmember/pages/regmembers/[id]/depositing.vue
+++ src/modules/mdulregmember/pages/regmembers/[id]/depositing.vue
@@ -37,6 +37,30 @@
 
 <script setup lang="ts">
 import { useQRCode } from '@vueuse/integrations/useQRCode';
+import * as Ably from 'ably';
+
+// For the full code sample see here: https://github.com/ably/quickstart-js
+const ably = new Ably.Realtime('9CNytA.ZRMqIg:YpI5Z9A8atb0cjkvlCvvGS8vvx8jg1clvIT6a0fhG_s');
+await ably.connection.once('connected');
+console.log('Connected to Ably!');
+
+const route = useRoute();
+async function handleConfirmation() {
+  navigateTo(`/regmembers/${route.params.id}/depositResult`);
+}
+
+// get the channel to subscribe to
+const channel = ably.channels.get('payment');
+
+/*
+  Subscribe to a channel.
+  The promise resolves when the channel is attached
+  (and resolves synchronously if the channel is already attached).
+*/
+await channel.subscribe('paid', (message) => {
+  console.log('Received a greeting message in realtime: ', message.data);
+  handleConfirmation();
+});
 
 const pageDef = useActiveModulePage('each.depositing');
 
