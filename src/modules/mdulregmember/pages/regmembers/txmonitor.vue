9b36e29c6edc1e9359b0b1f0a061a3a8391221e3
Teerachai Laothong <teerachai@finsoft2023.com>
2024-08-06 19:00:54 +0700

    feat: Implement real-time payment confirmation using Ably

--- src/modules/mdulregmember/pages/regmembers/txmonitor.vue
+++ src/modules/mdulregmember/pages/regmembers/txmonitor.vue
@@ -26,11 +26,38 @@
 </template>
 
 <script setup lang="ts">
+import * as Ably from 'ably';
+
 const pageDef = useActiveModulePage('list.txmonitor');
 useBreadcrumb('List');
 
-const { apiGet } = useHostApi(pageDef);
+const { apiGet, apiPost } = useHostApi(pageDef);
 const { data, error, pending } = apiGet();
+const { postResult, executePost } = apiPost();
+
+// For the full code sample see here: https://github.com/ably/quickstart-js
+const ably = new Ably.Realtime('9CNytA.ZRMqIg:YpI5Z9A8atb0cjkvlCvvGS8vvx8jg1clvIT6a0fhG_s');
+await ably.connection.once('connected');
+console.log('Connected to Ably!');
+
+const route = useRoute();
+async function handleIncomingTx(txdata: any) {
+  executePost(txdata);
+}
+
+// get the channel to subscribe to
+const channel = ably.channels.get('payment');
+
+/*
+  Subscribe to a channel.
+  The promise resolves when the channel is attached
+  (and resolves synchronously if the channel is already attached).
+*/
+await channel.subscribe('paid', (message) => {
+  const txdata = message.data;
+  console.log('Received a greeting message in realtime: ', message.data);
+  handleIncomingTx(txdata);
+});
 
 function selectItem(item: any) {
   navigateTo(`/regmembers/${item.id}`);
