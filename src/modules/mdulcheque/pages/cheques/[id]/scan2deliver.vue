4b23f769503887516b5067b25c714f314e23152f
Jane1m <rinda@finsoft2023.com>
2024-08-06 10:50:42 +0700

    add link QR Cose scan Page scan show QR code #27

--- src/modules/mdulcheque/pages/cheques/[id]/scan2deliver.vue
+++ src/modules/mdulcheque/pages/cheques/[id]/scan2deliver.vue
@@ -3,9 +3,7 @@
     <BPartPageTitle>{{ pageDef.label }}</BPartPageTitle>
 
     <BPartPageBody>
-      <UCard>
-        <DEntitySection v-model="data" :entries :pending />
-      </UCard>
+      <DShowQrCode qr-data="https://anycounter-428810.web.app/chooseInputCheck" />
     </BPartPageBody>
 
     <template #side>
@@ -14,12 +12,41 @@
   </BFullPage>
 </template>
 
-<script setup lang="ts">
-const pageDef = useActiveModulePage('each.scan2deliver');
+<script setup
+  lang="ts">
+  import * as Ably from 'ably';
 
-const { entries } = getEntrySchema(pageDef);
-const { apiGet } = useHostApi(pageDef);
-const { data, error, pending } = apiGet();
+  // For the full code sample see here: https://github.com/ably/quickstart-js
+  const ably = new Ably.Realtime('9CNytA.ZRMqIg:YpI5Z9A8atb0cjkvlCvvGS8vvx8jg1clvIT6a0fhG_s');
+  await ably.connection.once('connected');
+  console.log('Connected to Ably!');
 
-useBreadcrumb(pageDef.label);
+
+  // get the channel to subscribe to
+  const channel = ably.channels.get('linkup');
+
+  /*
+    Subscribe to a channel.
+    The promise resolves when the channel is attached
+    (and resolves synchronously if the channel is already attached).
+  */
+  await channel.subscribe('photo', (message) => {
+    console.log('Received a greeting message in realtime: ', message.data)
+    navigateTo('/');
+  });
+
+  const pageDef = useActiveModulePage('each.scan');
+
+  const { entries } = getEntrySchema(pageDef);
+  const { apiGet, apiPost } = useHostApi(pageDef);
+  const { data, error, pending } = apiGet();
+  const { postResult, executePost } = apiPost();
+
+  const route = useRoute();
+  async function handleConfirmation() {
+    await executePost({ state: 'completed', tstmp: { completed: new Date().toISOString() } });
+    navigateTo(`/withdrawals/${route.params.id}`);
+  }
+
+  useBreadcrumb(pageDef.label);
 </script>
