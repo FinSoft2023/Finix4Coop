ce443f8dfe3578e18ed536d040c9600551bb718d
Teerachai Laothong <teerachai@finsoft2023.com>
2024-08-06 11:30:12 +0700

    Hot fix bug

--- src/modules/mdulcheque/pages/cheques/[id]/scan.vue
+++ src/modules/mdulcheque/pages/cheques/[id]/scan.vue
@@ -15,46 +15,45 @@
   </BFullPage>
 </template>
 
-<script setup
-  lang="ts">
-  import * as Ably from 'ably';
+<script setup lang="ts">
+import * as Ably from 'ably';
 
-  // For the full code sample see here: https://github.com/ably/quickstart-js
-  const ably = new Ably.Realtime('9CNytA.ZRMqIg:YpI5Z9A8atb0cjkvlCvvGS8vvx8jg1clvIT6a0fhG_s');
-  await ably.connection.once('connected');
-  console.log('Connected to Ably!');
+// For the full code sample see here: https://github.com/ably/quickstart-js
+const ably = new Ably.Realtime('9CNytA.ZRMqIg:YpI5Z9A8atb0cjkvlCvvGS8vvx8jg1clvIT6a0fhG_s');
+await ably.connection.once('connected');
+console.log('Connected to Ably!');
 
 const route = useRoute();
-  async function handleConfirmation() {
+async function handleConfirmation() {
   await executePost({ state: 'deliver', tstmp: { deliver: new Date().toISOString() } });
   navigateTo(`/cheques/${route.params.id}`);
 }
 
-  // get the channel to subscribe to
-  const channel = ably.channels.get('linkup');
+// get the channel to subscribe to
+const channel = ably.channels.get('linkup');
 
-  /*
-    Subscribe to a channel.
-    The promise resolves when the channel is attached
-    (and resolves synchronously if the channel is already attached).
-  */
-  await channel.subscribe('photo', (message) => {
-    console.log('Received a greeting message in realtime: ', message.data)
-    navigateTo('/');
-  });
+/*
+  Subscribe to a channel.
+  The promise resolves when the channel is attached
+  (and resolves synchronously if the channel is already attached).
+*/
+await channel.subscribe('photo', (message) => {
+  console.log('Received a greeting message in realtime: ', message.data);
+  handleConfirmation();
+});
 
-  const pageDef = useActiveModulePage('each.scan');
+const pageDef = useActiveModulePage('each.scan');
 
-  const { entries } = getEntrySchema(pageDef);
-  const { apiGet, apiPost } = useHostApi(pageDef);
-  const { data, error, pending } = apiGet();
-  const { postResult, executePost } = apiPost();
+const { entries } = getEntrySchema(pageDef);
+const { apiGet, apiPost } = useHostApi(pageDef);
+const { data, error, pending } = apiGet();
+const { postResult, executePost } = apiPost();
 
-  const route = useRoute();
-  async function handleConfirmation() {
-    await executePost({ state: 'completed', tstmp: { completed: new Date().toISOString() } });
-    navigateTo(`/withdrawals/${route.params.id}`);
-  }
+// const route = useRoute();
+// async function handleConfirmation() {
+//   await executePost({ state: 'completed', tstmp: { completed: new Date().toISOString() } });
+//   navigateTo(`/withdrawals/${route.params.id}`);
+// }
 
-  useBreadcrumb(pageDef.label);
+useBreadcrumb(pageDef.label);
 </script>
