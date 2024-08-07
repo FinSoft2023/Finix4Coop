3ec3976904a56248a6ff275e6017ade92cfeb995
Teerachai Laothong <teerachai@finsoft2023.com>
2024-08-07 11:35:31 +0700

    refactor: Remove console.log statements and optimize confirm.post.ts

--- src/modules/mdulque/server/api/queues/create/confirm.post.ts
+++ src/modules/mdulque/server/api/queues/create/confirm.post.ts
@@ -1,8 +1,5 @@
 export default defineEventHandler(async (event) => {
-  // return await callPutToServer(event, '/api/queues/1234');
   const query = getQuery(event);
-  console.log('query', query);
-  console.log('query.id', query.id);
 
-  return [];
+  return await callPutToServer(event, `/api/queues/${query.id}`);
 });
