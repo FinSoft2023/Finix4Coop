69a3458561bc5a3caa172c4146c4ee841a0d5bbe
Jane1m <rinda@finsoft2023.com>
2024-08-07 14:47:53 +0700

    print withdrawals printdeposit #61

--- src/basis/finready/components/F/DocDepositsPrinting.vue
+++ src/basis/finready/components/F/DocDepositsPrinting.vue
@@ -1,39 +1,42 @@
-<template> 
-        <FPaperSize>
-          <div class="col-span-2 font-bold text-gray-900 dark:text-gray-900">
-            <p class="mt-[2mm] ml-[8cm] ">โรงพยาบาลศรีสะเกษ</p>
-          </div>
-          <div class="col-start-3 col-span-3">
-            <p class="mt-[1mm] ml-[3cm]">สหกรณ์ออมทรัพย์ </p>
-          </div>
-          <div class="flex items-center">
-            <p class="mt-[1mm] ml-[3cm] text-gray-900 font-bold dark:text-gray-90">01/03/2567</p>
-            <div class="flex items-center">
-              <div class="ml-[9cm]  text-gray-900 font-bold dark:text-gray-90">
-                <DFiLLCheck />
-              </div>
-            </div>
-          </div>
-          <div v-for="item in Details"
-            :key="item.id">
-            <div class="flex items-center">
-              <p class="mt-[1mm] ml-[6cm] text-gray-900 dark:text-gray-90">{{ item.name }}</p>
-              <p class="mt-[1mm] ml-[9cm] text-gray-900 dark:text-gray-90">{{ item.nember }}</p>
-            </div>
-            <div class="flex items-center">
-              <p class="mt-[1mm] ml-[2cm] text-gray-900 dark:text-gray-90">{{ item.amountcash }}</p>
-              <p class="mt-[1mm] ml-[13cm] text-gray-900 dark:text-gray-90">{{ item.amount }}</p>
-            </div>
-            <div class="flex items-center">
-              <p class="mt-[4cm] ml-[2cm] text-gray-900 dark:text-gray-90">{{ item.amountcash }}</p>
-              <p class="mt-[4cm] ml-[13cm] text-gray-900 dark:text-gray-90">{{ item.amount }}</p>
-            </div>
-          </div>
-        </FPaperSize>     
+<template>
+  <FPaperSize>
+    <div class="col-span-2 font-bold text-gray-900 dark:text-gray-900">
+      <p class="mt-[2mm] ml-[8cm] ">โรงพยาบาลศรีสะเกษ</p>
+    </div>
+    <div class="col-start-3 col-span-3">
+      <p class="mt-[1mm] ml-[3cm]">สหกรณ์ออมทรัพย์ </p>
+    </div>
+    <div class="flex datas-center">
+      <p class="mt-[1mm] ml-[3cm] text-gray-900 font-bold dark:text-gray-90">07/08/2567</p>
+      <div class="flex datas-center">
+        <div class="ml-[9cm]  text-gray-900 font-bold dark:text-gray-90">
+          <DFiLLCheck />
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="flex datas-center">
+        <p class="mt-[1mm] ml-[6cm] text-gray-900 dark:text-gray-90">{{ data.name }}</p>
+        <p class="mt-[1mm] ml-[9cm] text-gray-900 dark:text-gray-90">{{ data.accNo }}</p>
+      </div>
+      <div class="flex datas-center">
+        <p class="mt-[1mm] ml-[2cm] text-gray-900 dark:text-gray-90">{{ data.amount }}</p>
+        <p class="mt-[1mm] ml-[13cm] text-gray-900 dark:text-gray-90">{{ data.amount }}</p>
+      </div>
+      <div class="flex datas-center">
+        <p class="mt-[4cm] ml-[2cm] text-gray-900 dark:text-gray-90">{{ data.amount }}</p>
+        <p class="mt-[4cm] ml-[13cm] text-gray-900 dark:text-gray-90">{{ data.amount }}</p>
+      </div>
+    </div>
+  </FPaperSize>
 </template>
 
 <script setup
   lang="ts">
+  const pageDef = useActiveModulePage('each.printDeposit');
+
+  const { apiGet } = useHostApi(pageDef);
+  const { data, error, pending } = apiGet({ 'fltr-val': 'unuse' });
   const Details = ref([
     { name: 'นายสมใจ ดีใจ', nember: '094756375', amountcash: '800', amount: '800', total: '800' },
 
--- src/basis/finready/components/F/DocWithdrawsPrinting.vue
+++ src/basis/finready/components/F/DocWithdrawsPrinting.vue
@@ -2,23 +2,22 @@
   <FPaperSize>
     <div class="">
       <div class="">
+        <!-- <pre>{{ data }}</pre> -->
         <div class=" col-span-3 font-bold text-gray-900 dark:text-gray-900">
           <p class="mt-[2cm] ml-[8cm]">โรงพยาบาลศรีสะเกษ</p>
         </div>
         <p class="mt-[1mm] ml-[3cm] ">สหกรณ์ออมทรัพย์ </p>
-        <p class="mt-[1mm] ml-[3cm] text-gray-900 font-bold dark:text-gray-90">01/03/2567</p>
-        <div v-for="item in Details"
-          :key="item.id">
+        <p class="mt-[1mm] ml-[3cm] text-gray-900 font-bold dark:text-gray-90">07/08/2567</p>
+        <div>
           <div class="flex items-center">
-            <p class="mt-[1mm] ml-[6cm] text-gray-900 dark:text-gray-90">{{ item.name }}</p>
-            <p class="mt-[1mm] ml-[9cm] text-gray-900 dark:text-gray-90">{{ item.nember }}</p>
+            <p class="mt-[1mm] ml-[6cm] text-gray-900 dark:text-gray-90">{{ data.name }} </p>
+            <p class="mt-[1mm] ml-[9cm] text-gray-900 dark:text-gray-90">{{ data.accNo }}</p>
           </div>
           <div class="flex items-center">
             <p class="mt-[1mm] ml-[2cm] text-gray-900 dark:text-gray-90">
-              <MoneyText></MoneyText>
-              ตัวหนังสือ
+              <MoneyText>{{ data.amount }}</MoneyText>
             </p>
-            <p class="mt-[1mm] ml-[12cm] text-gray-900 dark:text-gray-90">{{ item.amount }}</p>
+            <p class="mt-[1mm] ml-[12cm] text-gray-900 dark:text-gray-90">{{ data.amount }}</p>
           </div>
         </div>
       </div>
@@ -28,7 +27,11 @@
 
 <script setup
   lang="ts">
-  const Details = ref([
-    { name: 'นายสมใจ ดีใจ', nember: '094756375', amountcash: '800', amount: '800', total: '800' },
-  ])
+  const pageDef = useActiveModulePage('each.print');
+
+  const { apiGet } = useHostApi(pageDef);
+  const { data, error, pending } = apiGet({ 'fltr-val': 'unuse' });
+  // const Details = ref([
+  //   { name: 'นายสมใจ ดีใจ', nember: '094756375', amountcash: '800', amount: '800', total: '800' },
+  // ])
 </script>
