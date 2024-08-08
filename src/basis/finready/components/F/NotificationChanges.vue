f463f56e19e0fa6709c03cabe890936b0a42b247
AhLei <138641969+9Kittipich@users.noreply.github.com>
2024-08-08 10:56:23 +0700

    chore: Refactor NotificationChanges.vue to use strict equality operator

--- src/basis/finready/components/F/NotificationChanges.vue
+++ src/basis/finready/components/F/NotificationChanges.vue
@@ -1,26 +1,26 @@
 <template>
 
-  <BOnPageNotification v-if="state = 'complate'"
+  <BOnPageNotification v-if="state === 'completed'"
     id="next1"
     label="บันทึกใบถอนเรียบร้อยเเล้ว"
     description="สามารถกดที่ปุ่ม 'ทำรายการถัดไป' ได้หากต้องการทำรายการคำขอถัดไป."
     actionText="ทำรายการถัดไป" />
-  <BOnPageNotification v-else-if="state = 'finances'"
+  <BOnPageNotification v-else-if="state === 'finance'"
     id="next2"
     label="ปริ้นเช็ค"
     description="สามารถกดที่ปุ่ม 'ปริ้นเช็ค' ได้หากต้องการทำรายการคำขอถัดไป."
     actionText="ปริ้นเช็ค" />
-  <BOnPageNotification v-else-if="state = 'active'"
+  <BOnPageNotification v-else-if="state === 'active'"
     id="next3"
     label="บันทึกเช็ค"
     description="สามารถกดที่ปุ่ม 'บันทึกเช็ค' ได้หากต้องการทำรายการคำขอถัดไป."
     actionText="บันทึกเช็ค" />
-  <BOnPageNotification v-else-if="state = 'deliver'"
+  <BOnPageNotification v-else-if="state === 'deliver'"
     id="next4"
     label="บันทึกขั่วเช็ค"
     description="สามารถกดที่ปุ่ม 'บันทึกขั่วเช็ค' ได้หากต้องการทำรายการคำขอถัดไป."
     actionText="บันทึกขั่วเช็ค" />
-  <BOnPageNotification v-else-if="state = 'account'"
+  <BOnPageNotification v-else-if="state === 'account'"
     id="next5"
     label="ปิดยอด"
     description="สามารถกดที่ปุ่ม 'ปิดยอด' ได้หากต้องการทำรายการคำขอถัดไป."
@@ -30,7 +30,7 @@
     label="ข้อมูลคำขอได้เข้าสู่ระบบแล้ว"
     description="สามารถกดที่ปุ่ม 'ปริ้นใบถอน' ได้หากต้องการปริ้นใบคำขอถอน."
     actionText="ปริ้นใบถอน" />
-
+{{ state }}
 </template>
 
 <script setup lang="ts">
@@ -40,7 +40,7 @@ const { entries } = getEntrySchema(pageDef);
 const { apiGet } = useHostApi(pageDef);
 const { data, error, pending } = apiGet();
 
-const state = computed(() => data.value.state);
+const state = computed(() => data.value?.state);
 
 
 
