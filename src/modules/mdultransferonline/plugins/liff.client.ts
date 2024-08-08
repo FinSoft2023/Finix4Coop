f85f1fa484f45188dd0332baf91f5b737c6cdb59
Teerachai Laothong <teerachai@finsoft2023.com>
2024-08-08 08:55:11 +0700

    feat: Add registration QR code page

---
+++ src/modules/mdultransferonline/plugins/liff.client.ts
@@ -0,0 +1,33 @@
+// This function is executed before instantiating the app
+// only in client-side.
+
+// import NPM version LIFF JS SDK
+import liff from "@line/liff";
+
+export default defineNuxtPlugin(() => {
+  // execute liff.init()
+  const initResult = liff
+    .init({ liffId: "2004942311-25EXrgMw" })
+    .then(() => {
+      // console.log("liff.init() done");
+    })
+    .catch((error) => {
+      console.log(`liff.init() failed: ${error}`);
+      // if (!process.env.liffId) {
+      //   console.info(
+      //     "LIFF Starter: Please make sure that you provided `LIFF_ID` as an environmental variable."
+      //   );
+      // }
+      return Promise.reject(error);
+    });
+
+  // You can access liff.init()'s return value (Promise object)
+  // as this.$liffInit() by inject()
+
+  return {
+    provide: {
+      liff,
+      liffInitResult: initResult,
+    },
+  };
+});
