91b8c91f06cb9ca379441f45bc58c4d8878db937
AhLei <138641969+9Kittipich@users.noreply.github.com>
2024-08-17 19:02:47 +0700

    feat: Update approve module sublinks

--- src/modules/mdulapprove/pages/approve/[id].vue
+++ src/modules/mdulapprove/pages/approve/[id].vue
@@ -8,9 +8,9 @@ const pageDef = useActiveModulePage('each.root');
 useComponentResolver(defaultViewResolvers);
 useSubLinks(
   [
-    { label: 'บันทึกชุดใบสมัคร', to: '/' },
-    { label: 'บันทึกใบยินยอมหักเงิน', to: '/edit' },
-    { label: 'ยกเลิกรายการ', to: '/edit' },
+    { label: 'บันทึกชุดใบสมัคร', to: '/scan1' },
+    { label: 'บันทึกใบยินยอมหักเงิน', to: '/scan2' },
+    { label: 'ยกเลิกรายการ', to: '/cancel' },
   ],
   pageDef,
 );
