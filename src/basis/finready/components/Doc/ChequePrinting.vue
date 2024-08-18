ced27110b322b249cf698b3ca111ebbf3f7fe1c8
AhLei <138641969+9Kittipich@users.noreply.github.com>
2024-08-18 17:47:03 +0700

    feat: Add QR code scanning functionality to ChequePrinting and Paper components

--- src/basis/finready/components/Doc/ChequePrinting.vue
+++ src/basis/finready/components/Doc/ChequePrinting.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="paperDoc w-[250mm] h-[91mm] p-[0mm] print:p-0 mx-auto print:m-0  mt-4 bg-white">
     <slot></slot>
+    <div class="text-right bottom-right text-boton">
+       <UIcon name="i-mdi-qrcode-scan" style="width: 50px; height: 50px"/>
+      </div>
   </div>
 </template>
+<style scoped>
+.paperDoc {
+  position: relative;
+}
+
+.bottom-right {
+  position: absolute;
+  right: 5mm;
+  bottom: 5mm;
+  text-align: right;
+}
+</style>
