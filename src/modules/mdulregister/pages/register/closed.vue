3cfc127cb5a5ad04528493fb526486c769578117
Jane1m <rinda@finsoft2023.com>
2024-08-21 21:38:33 +0700

    แก้ไขหน้าส่งรายชื่อ

--- src/modules/mdulregister/pages/register/closed.vue
+++ src/modules/mdulregister/pages/register/closed.vue
@@ -2,27 +2,44 @@
   <BFullPage>
     <BPartPageTitle>{{ pageDef.label }}</BPartPageTitle>
 
-    <BPartPageBody>
-      <BPartButtonsBand>
-        <!-- <UButton to="/register/create"
-          icon="i-heroicons-plus-circle">รับเรื่อง</UButton> -->
-        <UButton to="/register/closed"
+    <BPartPageBody>      
+      <div class="relative overflow-hidden bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
+      <div class="flex-row items-center justify-between p-4 space-y-3 sm:flex sm:space-y-0 sm:space-x-4">
+        <div>
+          <h5 class="mr-3 font-semibold dark:text-white">เลือกการส่งข้อมูลเพื่อตรวจสอบ</h5>
+        </div>
+        <div class="flex justify-end gap-4">
+
+          <UButton to="/register/closed"
           icon="i-heroicons-plus-circle">ยืนยันส่งเรื่องให้กับ รพ</UButton>
         <UButton to="/register/closed"
           icon="i-heroicons-plus-circle">Export เอกสาร</UButton>
+        </div>
+      </div>
+    </div>
+      <BPartButtonsBand>
+        <!-- <UButton to="/register/closed"
+          icon="i-heroicons-plus-circle">ยืนยันส่งเรื่องให้กับ รพ</UButton>
+        <UButton to="/register/closed"
+          icon="i-heroicons-plus-circle">Export เอกสาร</UButton> -->
 
         <!-- <template #next>
           <DSmartTabs />
         </template> -->
       </BPartButtonsBand>
-
       <BPartSectionTitle>{{ pageDef.label }}</BPartSectionTitle>
-
-      <NTableOfTxsListName @selection-changed="selectItem"
-        :data
-        :pending />
+      <UCard>
+        <NTableOfTxsListName @selection-changed="selectItem"
+          :data
+          :pending />
+      </UCard>
     </BPartPageBody>
+    <template #side>
+      <UCard></UCard>
+
+    </template>
   </BFullPage>
+
 </template>
 
 <script setup
