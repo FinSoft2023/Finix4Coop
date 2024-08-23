bd2fb0c8e56ac8ab2ab6f82b1ed9afd1e5918ad4
Jane1m <rinda@finsoft2023.com>
2024-08-23 14:44:26 +0700

    แก้ไขการทำงานให้ถ่ายรูป และเก็บลายเซ็นได้

--- src/modules/mdulofficer/pages/officer/[id]/takePhoto.vue
+++ src/modules/mdulofficer/pages/officer/[id]/takePhoto.vue
@@ -21,11 +21,50 @@
 </template>
 
 <script setup lang="ts">
-const pageDef = useActiveModulePage('each.takePhoto');
+import * as Ably from 'ably';
+
+// For the full code sample see here: https://github.com/ably/quickstart-js
+const ably = new Ably.Realtime('9CNytA.ZRMqIg:YpI5Z9A8atb0cjkvlCvvGS8vvx8jg1clvIT6a0fhG_s');
+await ably.connection.once('connected');
+console.log('Connected to Ably!');
+
+const imageUrl = ref('');
+const route = useRoute();
+async function handleConfirmation() {
+  await executePost({ state: 'completed', tstmp: { deliver: new Date().toISOString() } });
+  await $fetch(`/api/officer/${route.params.id}/images`, {
+    method: 'POST',
+    body: {
+      src: imageUrl.value,
+      alt: 'ลายเซ็น',
+    },
+  });
+  navigateTo(`/officer/${route.params.id}`);
+}
+
+// get the channel to subscribe to
+const channel = ably.channels.get('linkup');
+
+/*
+  Subscribe to a channel.
+  The promise resolves when the channel is attached
+  (and resolves synchronously if the channel is already attached).
+*/
+
+await channel.subscribe('photo', (message) => {
+  imageUrl.value = message.data.imageUrl;
+  console.log('Received a greeting message in realtime: ', message.data);
+
+});
+
+
+const pageDef = useActiveModulePage('each.scan');
 
 const { entries } = getEntrySchema(pageDef);
-const { apiGet } = useHostApi(pageDef);
+const { apiGet, apiPost } = useHostApi(pageDef);
 const { data, error, pending } = apiGet();
+const { postResult, executePost } = apiPost();
+
 
 useBreadcrumb(pageDef.label);
 </script>
