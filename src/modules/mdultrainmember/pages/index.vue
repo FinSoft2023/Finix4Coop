6df68a1004110ee157d9ca560c0be45a76b613e4
AhLei <138641969+9Kittipich@users.noreply.github.com>
2024-08-27 14:47:12 +0700

    generate module structure

---
+++ src/modules/mdultrainmember/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>(Trainmember) Index Page</h1>
+    <NuxtLink to="/trainmember">trainmember</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { setNavLinks } = useThemeSettings();
+setNavLinks([
+  { label: 'Trainmember', to: '/trainmember' },
+  { label: 'Create', to: '/trainmember/create' },
+]);
+</script>
