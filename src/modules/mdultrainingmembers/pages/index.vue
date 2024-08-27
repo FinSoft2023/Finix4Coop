3976bf9207ffe9b9277811c3ceb18ac266885855
baramee0191 <nobpharut@finsoft2023.com>
2024-08-27 14:55:53 +0700

    update gen module #170

---
+++ src/modules/mdultrainingmembers/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>(Trainingmembers) Index Page</h1>
+    <NuxtLink to="/trainingmembers">trainingmembers</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { setNavLinks } = useThemeSettings();
+setNavLinks([
+  { label: 'Trainingmembers', to: '/trainingmembers' },
+  { label: 'Create', to: '/trainingmembers/create' },
+]);
+</script>
