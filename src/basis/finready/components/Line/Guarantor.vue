65dbf5b2633e2d75e5beeaa7357751104d2a1f6f
AhLei <138641969+9Kittipich@users.noreply.github.com>
2024-09-07 13:13:47 +0700

    add component line app  #172

---
+++ src/basis/finready/components/Line/Guarantor.vue
@@ -0,0 +1,149 @@
+<template>
+  <!-- <p class="text-2xl p-4">สหกรณ์ออมทรัพโรงพยาบาลศรีสะเกษ จำกัด</p> -->
+  <p class="px-4 mt-4 text-xl text-gray-900 font-bold ">ข้อมูลค้ำประกัน</p>
+  <div class="space-y-4 mt-4">
+    <div class="grid grid-cols-1 gap-4 px-4 ">
+      <div class="flex flex-col gap-5">
+        <UCard>
+          <div label="Open">
+            <div class="text-gray-600">
+              <div class="grid grid-cols-2 gap-2">
+                <div class="col-span-2 flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg"
+                    class="text-primary-400"
+                    width="2em"
+                    height="2em"
+                    viewBox="0 0 48 48">
+                    <g fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="4">
+                      <circle cx="24"
+                        cy="12"
+                        r="8" />
+                      <path d="M42 44c0-9.941-8.059-18-18-18S6 34.059 6 44" />
+                    </g>
+                  </svg>
+                  <p class="flex justify-center text-md mt-0 font-bold mx-3">KITTISAK SENADEE
+                  </p>
+                </div>
+                <div>
+                  <p class="text-md mx-3 text-gray-400 ">ทะเบียนสัญญาที่ค้ำ</p>
+                  <p class="text-md font-bold mx-3">ส 58/58 </p>
+                </div>
+                <div>
+                  <p class="text-md mx-3 text-gray-400">วงเงินกู้</p>
+                  <p class="text-md font-bold mx-3">45,000 บาท</p>
+                </div>
+                <div>
+                  <p class="text-md mx-3 text-gray-400 ">วันที่กู้</p>
+                  <p class="text-md font-bold mx-3">03/11/2557</p>
+                </div>
+                <div>
+                  <p class="text-md mx-3 text-gray-400 ">ยอดคงเหลือ</p>
+                  <p class="text-md font-bold mx-3">41,000 บาท</p>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </UCard>
+      </div>
+      <div>
+        <UCard>
+          <div label="Open">
+            <div class="text-gray-600">
+              <div class="grid grid-cols-2 gap-2">
+                <div class="col-span-2 flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg"
+                    class="text-primary-400"
+                    width="2em"
+                    height="2em"
+                    viewBox="0 0 48 48">
+                    <g fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="4">
+                      <circle cx="24"
+                        cy="12"
+                        r="8" />
+                      <path d="M42 44c0-9.941-8.059-18-18-18S6 34.059 6 44" />
+                    </g>
+                  </svg>
+                  <p class="flex justify-center text-md mt-0 font-bold mx-3">AHPICAI MAMUANG
+                  </p>
+                </div>
+                <div>
+                  <p class="text-md mx-3 text-gray-400 ">ทะเบียนสัญญาที่ค้ำ</p>
+                  <p class="text-md font-bold mx-3">ส 58/58 </p>
+                </div>
+                <div>
+                  <p class="text-md mx-3 text-gray-400">วงเงินกู้</p>
+                  <p class="text-md font-bold mx-3">45,000 บาท</p>
+                </div>
+                <div>
+                  <p class="text-md mx-3 text-gray-400 ">วันที่กู้</p>
+                  <p class="text-md font-bold mx-3">03/11/2557</p>
+                </div>
+                <div>
+                  <p class="text-md mx-3 text-gray-400 ">ยอดคงเหลือ</p>
+                  <p class="text-md font-bold mx-3">41,000 บาท</p>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </UCard>
+      </div>
+      <div>
+        <UCard>
+          <div>
+            <div class="text-gray-600">
+              <div class="grid grid-cols-2 gap-2">
+                <div class="col-span-2 flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg"
+                    class="text-primary-400"
+                    width="2em"
+                    height="2em"
+                    viewBox="0 0 48 48">
+                    <g fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="4">
+                      <circle cx="24"
+                        cy="12"
+                        r="8" />
+                      <path d="M42 44c0-9.941-8.059-18-18-18S6 34.059 6 44" />
+                    </g>
+                  </svg>
+                  <p class="flex justify-center text-md mt-0 font-bold mx-3">RATEE MEDEE
+                  </p>
+                </div>
+                <div>
+                  <p class="text-md mx-3 text-gray-400 ">ทะเบียนสัญญาที่ค้ำ</p>
+                  <p class="text-md font-bold mx-3">ส 58/58 </p>
+                </div>
+                <div>
+                  <p class="text-md mx-3 text-gray-400">วงเงินกู้</p>
+                  <p class="text-md font-bold mx-3">45,000 บาท</p>
+                </div>
+                <div>
+                  <p class="text-md mx-3 text-gray-400 ">วันที่กู้</p>
+                  <p class="text-md font-bold mx-3">03/11/2557</p>
+                </div>
+                <div>
+                  <p class="text-md mx-3 text-gray-400 ">ยอดคงเหลือ</p>
+                  <p class="text-md font-bold mx-3">41,000 บาท</p>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </UCard>
+      </div>
+    </div>
+  </div>
+</template>
