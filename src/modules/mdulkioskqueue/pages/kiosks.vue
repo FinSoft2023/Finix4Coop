224084980f1ec534c688d2f7473d748b72e05e50
AhLei <138641969+9Kittipich@users.noreply.github.com>
2024-10-04 13:09:34 +0700

    Add tsconfig.json files for mdulkioskqueue module

---
+++ src/modules/mdulkioskqueue/pages/kiosks.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtPage />
+</template>
+
+<script setup lang="ts">
+const moduleRef = useKiosksModule();
+setModulePageRoot([
+  {
+    label: 'Home',
+    icon: 'i-heroicons-home',
+    to: '/',
+  },
+  {
+    label: 'Kiosks',
+    icon: 'i-heroicons-square-3-stack-3d',
+    to: '/kiosks',
+  },
+]);
+useSmartTabs([
+  {
+    label: 'Active',
+    to: '/kiosks',
+  },
+  {
+    label: 'Closed',
+    to: '/kiosks/closed',
+  },
+]);
+</script>
