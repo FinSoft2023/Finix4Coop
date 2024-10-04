cb1e71a7f54e8c3c5b8e23cd517d7f08e2b0e990
AhLei <138641969+9Kittipich@users.noreply.github.com>
2024-10-04 13:33:57 +0700

    Refactor kioskqueue.module.ts to remove redundant subName properties

--- src/blueprint/kioskqueue.module.ts
+++ src/blueprint/kioskqueue.module.ts
@@ -11,31 +11,26 @@ export const moduleBlueprint = defineModule('kiosks', 'คู้คิว', {
       entity: kiosk,
       post: true,
       role: 'stepedit',
-      subName: 'account',
     },
     takephoto: {
       entity: kiosk,
       post: true,
       role: 'stepedit',
-      subName: 'account',
     },
     choice: {
       entity: kiosk,
       post: true,
       role: 'stepedit',
-      subName: 'account',
     },
     scankiosk: {
       entity: kiosk,
       post: true,
       role: 'stepedit',
-      subName: 'account',
     },
     confirm: {
       entity: kiosk,
       post: true,
       role: 'stepconfirm',
-      subName: 'account',
     },
     steps: {
       entity: kiosk,
