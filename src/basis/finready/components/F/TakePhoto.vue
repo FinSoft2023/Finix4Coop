d3f8893433ce1c845a4ec398bf9282018d7af93e
AhLei <138641969+9Kittipich@users.noreply.github.com>
2024-10-05 14:15:05 +0700

    Refactor TakePhoto and Welcome components

--- src/basis/finready/components/F/TakePhoto.vue
+++ src/basis/finready/components/F/TakePhoto.vue
@@ -2,14 +2,93 @@
   <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       <div class="mx-auto max-w-screen-sm text-center">
-        <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
-          Welcome
-        </h1>
-        <p class="mb-8 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
-          สหกรณ์ออมทรัพย์โรงพยาบาลศรีเกษยินดีต้อนรับ
+        <p v-if="!photoTaken"
+          class="mb-4 text-2xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+          ถ่ายภาพ ยืนยันตัวตน
         </p>
-  
+        <p v-if="photoTaken"
+          class="mb-4 text-2xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+          ภาพถ่ายของท่าน
+        </p>
+
+        <div>
+          <video v-show="!photoTaken"
+            ref="video"
+            width="640"
+            height="480"
+            autoplay></video>
+          <canvas v-show="photoTaken"
+            ref="canvas"
+            width="640"
+            height="480"></canvas>
+        </div>
+
+        <div class="mt-5"
+          v-if="!photoTaken">
+          <UButton @click="takePhoto"
+            size="xl"
+            variant="solid"
+            icon="i-heroicons-camera"
+            :trailing="true">
+            ถ่ายภาพ
+          </UButton>
+        </div>
+
+        <div class="mt-5"
+          v-if="photoTaken">
+          <UButton @click="retakePhoto"
+            class="mr-5"
+            size="xl"
+            variant="solid"
+            icon="i-heroicons-arrow-uturn-left"
+            :trailing="true">
+            ถ่ายภาพใหม่
+          </UButton>
+          <UButton size="xl"
+            variant="solid"
+            icon="i-heroicons-arrow-right"
+            :trailing="true"
+            to="/kiosks/create/choice">
+            ดำเนินการต่อ
+          </UButton>
+        </div>
       </div>
     </div>
   </section>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const photoTaken = ref(false);
+const video = ref(null);
+const canvas = ref(null);
+
+const startVideo = async () => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.value.srcObject = stream;
+  } catch (err) {
+    console.error("Error accessing the camera:", err);
+  }
+};
+
+const takePhoto = () => {
+  const context = canvas.value.getContext('2d');
+  context.drawImage(video.value, 0, 0, 640, 480);
+  photoTaken.value = true;
+
+  const stream = video.value.srcObject;
+  const tracks = stream.getTracks();
+  tracks.forEach(track => track.stop());
+};
+
+const retakePhoto = () => {
+  photoTaken.value = false;
+  startVideo();
+};
+
+onMounted(() => {
+  startVideo();
+});
+</script>
--- src/basis/finready/components/F/Welcome.vue
+++ src/basis/finready/components/F/Welcome.vue
@@ -1,15 +1,26 @@
 <template>
   <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div class="flex justify-center mb-10">
+        <img class="w-[50mm] h-[50mm] max-w-full text-red-300 rounded-lg"
+          src="https://www.saving-sskh.com/images/logo-saving.png"
+          alt="">
+      </div>
       <div class="mx-auto max-w-screen-sm text-center">
-        <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
-          Welcome
+        <h1
+          class="mb-8 text-center text-4xl tracking-tight font-extrabold lg:text-6xl text-blue-400 dark:text-primary-500">
+          ยินดีต้อนรับ
         </h1>
-        <p class="mb-8 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
-          สหกรณ์ออมทรัพย์โรงพยาบาลศรีเกษยินดีต้อนรับ
+        <p class="mb-4 text-center text-2xl tracking-tight font-bold text-gray-900 md:text-3xl dark:text-white">
+          สหกรณ์ออมทรัพย์โรงพยาบาลศรีสะเกษ จำกัด
         </p>
-        <UButton to="/kiosks/create"
-          size="xl">ดำเนินการ</UButton>
+        <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">สามารถทำรายการได้โดยการกดที่ปุ่มด้านล่าง</p>
+        <a href="/kiosks/create"
+          class="my-16 flex justify-center">
+          <button type="button"
+            class="px-8 py-3.5 text-2xl font-medium text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">รับคิว
+            ติดต่อสหกรณ์</button>
+        </a>
       </div>
     </div>
   </section>
