b8af45d45633b9ff0b1118c4c592672b583c7fb1
AhLei <138641969+9Kittipich@users.noreply.github.com>
2024-10-05 14:16:09 +0700

    Refactor kioskqueue.module.ts and add role property to moduleBlueprint

--- src/blueprint/kioskqueue.module.ts
+++ src/blueprint/kioskqueue.module.ts
@@ -10,14 +10,17 @@ export const moduleBlueprint = defineModule('kiosks', 'คู้คิว', {
     root: {
       entity: kiosk,
       post: true,
+      role: 'stepedit',
     },
     choice: {
       entity: kiosk,
       post: true,
+      role: 'stepedit',
     },
     scankiosk: {
       entity: kiosk,
       post: true,
+      role: 'stepedit',
     },
     confirm: {
       entity: kiosk,
