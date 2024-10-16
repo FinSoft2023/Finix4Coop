44fe60607cd4e580d1dd81592011ff829059b17e
Jane1m <rinda@finsoft2023.com>
2024-10-16 13:00:06 +0700

    เพิ่มหน้า สมัครสมาชิก

--- src/modules/mdulbindanaccount/pages/index.vue
+++ src/modules/mdulbindanaccount/pages/index.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
-    <h1>(Bindanaccount) Index Page</h1>
-    <NuxtLink to="/bindanaccount">bindanaccount</NuxtLink>
+    <!-- <h1>(Bindanaccount) Index Page</h1>
+    <NuxtLink to="/bindanaccount">bindanaccount</NuxtLink> -->
+    <div class="flex justify-center mt-8">
+      <Icon
+        class="w-20 h-20 text-green-400"
+        name="i-mdi-user"
+      ></Icon>
+    </div>
+    <h2 class="mt-4 mb-4 text-2xl text-center font-bold dark:text-white mt-4">
+      สมัครสมาชิกให่ใช่หรือไม่?
+    </h2>
+    <h3 class="text-center text-gray-500 dark:text-gray-300 mt-4">
+      ขอเราทำความรู้จักคุณหน่อยได้ไหม
+    </h3>
+    <div class="flex justify-center mt-8">
+      <UButton to="/bindanaccount/create">ยินยอม</UButton>
+    </div>
   </div>
 </template>
 
