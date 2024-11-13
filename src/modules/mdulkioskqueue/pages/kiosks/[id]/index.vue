4bf50b4ad3040c11fa59f4670f602efa72f2b4e5
Jane1m <rinda@finsoft2023.com>
2024-11-13 16:51:36 +0700

    ""

--- src/modules/mdulkioskqueue/pages/kiosks/[id]/index.vue
+++ src/modules/mdulkioskqueue/pages/kiosks/[id]/index.vue
@@ -9,15 +9,11 @@
         </h2> -->
       <section class="bg-white dark:bg-gray-900">
         <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-          <h1
-            class="mb-4 text-center text-3xl tracking-tight font-extrabold lg:text-4xl text-gray-900 dark:text-white"
-          >
+          <h1 class="mb-4 text-center text-3xl tracking-tight font-extrabold lg:text-4xl text-gray-900 dark:text-white">
             บัตรคิว
-          </h1>       
+          </h1>
           <div class="mx-auto max-w-screen-sm text-center">
-            <h1
-              class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500"
-            >
+            <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
               A-002
             </h1>
             <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
@@ -27,7 +23,11 @@
         </div>
       </section>
       <div class="flex justify-center mt-0">
-        <UButton class="mr-5" size="xl" variant="solid" :trailing="true" to="">
+        <UButton size="xl"
+          class="py-2.5 px-20 me-2 mb-2"
+          variant="solid"
+          :trailing="true"
+          to="/kiosks">
           ตกลง
         </UButton>
         <!-- <UButton size="xl" variant="solid" :trailing="true">
