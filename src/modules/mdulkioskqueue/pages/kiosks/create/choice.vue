f949f9a295fd375d1a846f0f89fffdfd66807ad0
AhLei <138641969+9Kittipich@users.noreply.github.com>
2024-11-24 14:16:20 +0700

    เพิ่มคอมโพเนนต์ Numpad สำหรับกรอกเลขสมาชิกในหน้า Choice

--- src/modules/mdulkioskqueue/pages/kiosks/create/choice.vue
+++ src/modules/mdulkioskqueue/pages/kiosks/create/choice.vue
@@ -1,35 +1,14 @@
 <template>
   <BFullPage>
-    <!-- <BPartPageTitle>{{ pageDef.label }}</BPartPageTitle> -->
-
     <BPartPageBody>
-      <section class="bg-white dark:bg-gray-900">
-        <div class="py-8 px-4 mx-auto max-w-screen-md lg:py-4 lg:px-6">
-          <UForm @submit="handleSubmit"
-            :state="data"
-            :schema="schema"
-            :pending="pending"
-            class="space-y-4">
-            <h1
-              class="mb-4 text-center text-3xl tracking-tight font-extrabold lg:text-3xl text-gray-900 dark:text-white">
-              กรุณากรอกเลขสมาชิกของท่าน
-            </h1>
-            <h1 class="mb-4 text-center text-2xl tracking-tight font-light lg:text-2xl text-gray-900 dark:text-white">
-              หากยังไม่มีเลขสมาชิกสามารถกดปุ่ม "ยังไม่เป็นสมาชิก" ด้านล่าง
-            </h1>
-            <DEntitySection v-model="data"
-              :entries="entries"
-              :pending />
-            <div class="mt-8">
-              <ULink to="/components/link"
-                active-class="text-green"
-                inactive-class="mr-5 text-green-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
-                ยังไม่เป็นสมาชิก
-              </ULink>
-            </div>
-          </UForm>
-        </div>
-      </section>
+     <FNumpad></FNumpad>
+      <div class="mt-8 text-center">
+        <ULink to="/kiosks/create/scankiosk"
+          active-class="text-green"
+          inactive-class="text-green-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">
+          ยังไม่เป็นสมาชิก
+        </ULink>
+      </div>
     </BPartPageBody>
     <BPartButtonsBand>
       <UButton class="mr-5"
@@ -40,12 +19,6 @@
         ย้อนกลับ
       </UButton>
       <template #next>
-        <!-- <UButton class="mr-4"
-          size="xl"
-          variant="solid"
-          to="/kiosks/create/scankiosk">
-          ยังไม่มีรหัสสมาชิก
-        </UButton> -->
         <UButton size="xl"
           variant="solid"
           icon="i-heroicons-chevron-right-16-solid"
@@ -59,17 +32,13 @@
 </template>
 
 <script setup lang="ts">
-// import type { z } from 'zod';
-
 const pageDef = useActiveModulePage('create.choice');
 useBreadcrumb('Create');
 
-const { entries, schema } = getEntrySchema(pageDef);
-// type TSchema = z.output<typeof schema>;
-
 const { apiGet, apiPost } = useHostApi(pageDef);
-const { data, error, pending } = apiGet();
+const { data, pending } = apiGet();
 const { postResult, executePost } = apiPost();
+
 
 const handleSubmit = async () => {
   await executePost(data.value);
