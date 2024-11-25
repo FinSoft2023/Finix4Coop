e4838b601d585c3ac887e6560ce8c1bf1ccf1dc2
AhLei <138641969+9Kittipich@users.noreply.github.com>
2024-11-25 19:47:12 +0700

    สร้างคอมโพเนนต์ PreparePhoto.vue และปรับปรุงหน้า preparephoto.vue เพื่อแสดงผลการเตรียมตัวถ่ายภาพ

--- src/modules/mdulkioskqueue/pages/kiosks/[id]/addline.vue
+++ src/modules/mdulkioskqueue/pages/kiosks/[id]/addline.vue
@@ -16,47 +16,11 @@
               :key="index"
               class="text-red-500"
               v-bind="module">
-              <!-- <template #description>
-                <span class="line-clamp-2">{{ module.description }}</span>
-              </template> -->
             </UPageCard>
           </DItemGrid>
         </div>
       </section>
-      <!-- <section class="bg-white dark:bg-gray-900">
-        <div class="py-8 px-4 mx-auto max-w-screen-md lg:py-16 lg:px-6">
-          <h1
-            class="mb-8 text-center text-3xl tracking-tight font-extrabold lg:text-3xl text-gray-900 dark:text-white"
-          >
-            คุณต้องการรับคิวจากช่องทางไหน ?
-          </h1>
-
-          <div class="flex justify-center space-x-4 mt-4">
-            <UButton
-              class="mr-5"
-              size="xl"
-              variant="solid"
-              :trailing="true"
-              to="/kiosks/create/choice"
-            >
-              โทรศัพท์
-            </UButton>
-            <UButton
-              size="xl"
-              variant="solid"
-              :trailing="true"
-              to="/kiosks/create/scankiosk"
-            >
-              จากตู้ (ไม่มีโทรศัพท์)
-            </UButton>
-          </div>
-        </div>
-      </section> -->
     </BPartPageBody>
-
-    <!-- <template #side>
-      <DSubLinks />
-    </template> -->
   </BFullPage>
 </template>
 
