98bfff3d8e111b4f04939c0af25cd8e7f7c95d9e
baramee0191 <nobpharut@finsoft2023.com>
2024-11-25 11:47:47 +0700

    เพิ่มคอมโพเนนต์ MemberInfoCard และ Numpad; ปรับปรุงหน้า ChoiceContinue.vue และ memberinfo.vue #189

---
+++ src/modules/mdulkioskqueue/pages/kiosks/[id]/memberinfo.vue
@@ -0,0 +1,35 @@
+<template>
+  <BFullPage>
+    <BPartPageBody>
+      <FMemberInfoCard :memberData="memberData" />
+    </BPartPageBody>
+    <BPartButtonsBand>
+      <UButton class="mr-5"
+        size="xl"
+        variant="outline"
+        icon="i-heroicons-chevron-left-16-solid"
+        to="/kiosks/create/choice">
+        ย้อนกลับ
+      </UButton>
+      <template #next>
+        <UButton size="xl"
+          variant="solid"
+          icon="i-heroicons-chevron-right-16-solid"
+          :trailing="true"
+          to="/kiosks/create/scankiosk">
+          ถัดไป
+        </UButton>
+      </template>
+    </BPartButtonsBand>
+  </BFullPage>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const memberData = reactive({
+  id: route.query.id || "2001",
+  name: "นายตัวxxx สมxx",
+  status: "สมาชิก",
+  registrationDate: "10 มกราคม 2564",
+});
+</script>
