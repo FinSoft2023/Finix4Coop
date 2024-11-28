3e8221225a84b6779bfd3f5ee1ab6e609a4f6bec
baramee0191 <nobpharut@finsoft2023.com>
2024-11-28 17:08:21 +0700

    update concept #186

---
+++ src/modules/mdulloansharemember/pages/loansharemember/create/fillin2.vue
@@ -0,0 +1,73 @@
+<template>
+  <BFullPage>
+    <BPartPageTitle>{{ pageDef.label }}</BPartPageTitle>
+    <UAlert
+      icon="i-heroicons-book-open"
+      description="กรอกข้อมูลสำหรับตรวจสอบข้อมูล"
+      :title="pageDef.label"
+    />
+
+    <BPartPageBody>
+      <UForm
+        @submit="handleSubmit"
+        :state="data"
+        :schema="schema"
+        :pending="pending"
+        class="space-y-4"
+      >
+        <UCard class="col-span-2">
+          <h5 class="text-md font-bold leading-none text-gray-900 dark:text-white">
+              รายจ่ายของคุณ
+            </h5>
+        <p class="mt-3 mb-1 text-md">ค่าไฟ (บาท)</p>
+        <UInput v-model="value" />
+        <p class="mt-3 mb-1 text-md">ค่าน้ำ (บาท)</p>
+        <UInput v-model="value" />
+        <p class="mt-3 mb-1 text-md">หนี้ กยส. (บาท)</p>
+        <UInput v-model="value" />
+        <p class="mt-3 mb-1 text-md">หนี้ กบข. (บาท)</p>
+        <UInput v-model="value" />
+        <p class="mt-3 mb-1 text-md">หนี้อื่นๆ (บาท)</p>
+        <UInput v-model="value" />
+
+        </UCard>
+        <div class="flex justify-end">
+          <UButton @click="navigateTo('/loansharemember/create')">บันทึก</UButton>
+        </div>
+
+
+
+        <BPartButtonsBand>
+          <!-- <template #next>
+            <UButton type="submit">บันทึก</UButton>
+          </template> -->
+        </BPartButtonsBand>
+      </UForm>
+    </BPartPageBody>
+  </BFullPage>
+</template>
+
+<script setup lang="ts">
+// import type { z } from 'zod';
+
+const pageDef = useActiveModulePage('create.root');
+useSmartStepper(pageDef);
+useBreadcrumb('Create');
+
+const { entries, schema } = getEntrySchema(pageDef);
+// type TSchema = z.output<typeof schema>;
+
+const { apiGet, apiPost } = useLocalStage(pageDef);
+const { data, pending } = apiGet();
+const { postResult, error, executePost } = apiPost();
+
+// If this is the first step, you can initialize the data value like this:
+// data.value = {};
+
+const handleSubmit = async () => {
+  await executePost(data.value);
+  const redirectPath = getNextStep(pageDef);
+  navigateTo(`/loansharemember/create/${redirectPath}`);
+};
+const value = ref('10000')
+</script>
--- src/modules/mdulloansharemember/pages/loansharemember/create/index.vue
+++ src/modules/mdulloansharemember/pages/loansharemember/create/index.vue
@@ -3,7 +3,7 @@
     <BPartPageTitle>{{ pageDef.label }}</BPartPageTitle>
     <UAlert
       icon="i-heroicons-book-open"
-      description="ตรวจสอบสิทธิขอกู้"
+      description="กรอกข้อมูลสำหรับตรวจสอบข้อมูล"
       :title="pageDef.label"
     />
 
@@ -15,67 +15,172 @@
         :pending="pending"
         class="space-y-4"
       >
-        <UCard class="col-span-2">
-          <!-- <h2 class="mt-4 mb-4 text-2xl text-center font-bold dark:text-white">
-          คิว A1
-        </h2> -->
-      <section class="bg-white dark:bg-gray-900">
-        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-  <h1
-    class="mb-4 text-center text-3xl tracking-tight font-extrabold lg:text-4xl text-gray-900 dark:text-white"
-  >
-    ตรวจสอบขอกู้
-  </h1>
-  <div class="mx-auto max-w-screen-sm text-center">
-    <h1
-      class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500 flex items-center justify-center space-x-4"
-    >
-    <!-- <UIcon name="i-heroicons-clipboard-document-check"
-    class="w-32 h-32 text-green-500 dark:text-green-400"></UIcon> -->
-    <!-- <img src="https://www.saving-sskh.com/images/logo-saving.png" alt="Logo" style="width: 50%; height: auto;" class="mx-auto mb-3 mt-3" /> -->
-    <UIcon name="i-heroicons-document-magnifying-glass"
-    class="w-32 h-32 text-green-500 dark:text-green-500 "></UIcon>
-    </h1>
-    <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-      กรุณาทำการตรวจสอบขอกู้ก่อนเพื่อตรวจสอบข้อมูลของท่านกับเงื่อนไขของสหกรณ์
-    </p>
-    <div class="flex justify-center mt-5">
-        <UButton type="submit">เริ่มตรวจสอบ</UButton>
-      </div>
-  </div>
-</div>
-      </section>
+        <UCard>
+          <div class="flex items-center justify-between mb-4">
+            <h5 class="text-md font-bold leading-none text-gray-500 dark:text-white">
+              รายรับของคุณ
+            </h5>
+            <div class="flex items-center space-x-0.5">
+              <UIcon
+                name="i-heroicons-pencil-square"
+                class="w-5 h-5 text-green-500 dark:text-green-400"
+              ></UIcon>
+              <a
+                @click="navigateTo('/loansharemember/create/fillin')"
+                class="text-sm font-bold text-green-600 hover:underline dark:text-green-500"
+              >
+                เปลี่ยน
+              </a>
+            </div>
+          </div>
+
+          <div class="flex justify-start">
+            <h5 class="text-md font-bold leading-none text-gray-900 dark:text-white">
+              100,000 บาท
+            </h5>
+          </div>
+          <hr class="border-gray-300 my-4 mt-5" />
+          <div class="flex items-center justify-between mb-4">
+            <h5 class="text-md font-bold leading-none text-gray-500 dark:text-white">
+              รายจ่ายของคุณ
+            </h5>
+            <div class="flex items-center space-x-0.5">
+              <UIcon
+                name="i-heroicons-pencil-square"
+                class="w-5 h-5 text-green-500 dark:text-green-400"
+              ></UIcon>
+              <a
+                @click="navigateTo('/loansharemember/create/fillin2')"
+                class="text-sm font-bold text-green-600 hover:underline dark:text-green-500"
+              >
+                เปลี่ยน
+              </a>
+            </div>
+          </div>
+
+          <div class="flex justify-start">
+            <h5 class="text-md font-bold leading-none text-gray-900 dark:text-white">
+              50,000 บาท
+            </h5>
+          </div>
+          <hr class="border-gray-300 my-4 mt-5" />
+          <div class="flex justify-center">
+            <UButton @click="showCard = true">ตรวจสอบขอกู้</UButton>
+          </div>
         </UCard>
-        <BPartButtonsBand>
-          <template #next>
-            <!-- <UButton type="submit">ตรวจสอบขอกู้</UButton> -->
-          </template>
-        </BPartButtonsBand>
+        <UCard v-if="showCard" class="mt-4">
+        <p class="text-center text-green-600 font-bold mb-5">ผ่านเงื่อนไขแล้ว</p>
+        <UAccordion :items="items" variant="soft">
+    <template #item="{ item }">
+      <p class="italic text-gray-900 dark:text-white text-center">
+        {{ item.description }}
+      </p>
+    </template>
+
+    <template #getting-started>
+      <div class="text-gray-900 dark:text-white text-center">
+        <Logo class="w-auto h-8 mx-auto" />
+
+        <p class="text-sm text-gray-500 dark:text-gray-400 mt-2">
+         
+          <ul class="space-y-2">
+  <li class="flex items-center text-gray-500 dark:text-gray-400 text-md">
+    <div class="ml-3 w-4 h-4 bg-green-500 rounded-full flex items-center justify-center mr-3">
+      <svg
+        class="w-3 h-3 text-white"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </div>
+    ระยะเวลาการเป็นสมาชิก
+  </li>
+  <li class="flex items-center text-gray-500 dark:text-gray-400 text-md">
+    <div class="ml-3 w-4 h-4 bg-green-500 rounded-full flex items-center justify-center mr-3">
+      <svg
+        class="w-3 h-3 text-white"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </div>
+    รายได้ต่อเดือน
+  </li>
+  <li class="flex items-center text-gray-500 dark:text-gray-400 text-md">
+    <div class="ml-3 w-4 h-4 bg-green-500 rounded-full flex items-center justify-center mr-3">
+      <svg
+        class="w-3 h-3 text-white"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </div>
+    ตำแหน่งงาน
+  </li>
+</ul>
+
+
+        </p>
+      </div>
+      
+    </template>
+  </UAccordion>
+  <BPartButtonsBand>
+    <template #next>
+      <UButton type="submit">ขอกู้</UButton>
+    </template>
+  </BPartButtonsBand>
+      </UCard>
       </UForm>
+
+    
     </BPartPageBody>
+    
   </BFullPage>
 </template>
 
 <script setup lang="ts">
-// import type { z } from 'zod';
+import { ref } from 'vue';
 
 const pageDef = useActiveModulePage('create.root');
 useSmartStepper(pageDef);
 useBreadcrumb('Create');
 
 const { entries, schema } = getEntrySchema(pageDef);
-// type TSchema = z.output<typeof schema>;
 
 const { apiGet, apiPost } = useLocalStage(pageDef);
 const { data, pending } = apiGet();
 const { postResult, error, executePost } = apiPost();
-
-// If this is the first step, you can initialize the data value like this:
-// data.value = {};
 
 const handleSubmit = async () => {
   await executePost(data.value);
   const redirectPath = getNextStep(pageDef);
   navigateTo(`/loansharemember/create/${redirectPath}`);
 };
+
+const showCard = ref(false);
+const items = [{
+  label: 'เงื่อนไขการขอสินเชื่อ',
+  icon: 'i-heroicons-information-circle',
+  defaultOpen: true,
+  slot: 'getting-started'
+},]
 </script>
