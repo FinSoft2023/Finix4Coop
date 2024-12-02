18ffbbfdfa603441a59c8c2b0b89b0d59b0b5b5d
AhLei <138641969+9Kittipich@users.noreply.github.com>
2024-12-02 16:11:31 +0700

    Add loangeneralmember module with API endpoints and configuration files

---
+++ src/modules/mdulloangeneralmember/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>(Loangeneralmember) Index Page</h1>
+    <NuxtLink to="/loangeneralmember">loangeneralmember</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { setNavLinks } = useThemeSettings();
+setNavLinks([
+  { label: 'Loangeneralmember', to: '/loangeneralmember' },
+  { label: 'Create', to: '/loangeneralmember/create' },
+]);
+</script>
